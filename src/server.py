af0b9399e9d055378162b6e86c9e68239257258c
Orlando <opseua@gmail.com>
2024-07-29 09:06:20 -0300

    Padrão de pastas e no servidor HTTP

--- src/server.py
+++ src/server.py
@@ -13,186 +13,205 @@
 # pylint: disable=W0603
 # pylint: disable=C0115
 # pylint: disable=R1710
+# ERRO DE IMPORT EM OUTRA PASTA
+# pylint: disable=E0401
+# ERRO DE IMPORT ANTES DE USAR A VARIÁVEL
+# pylint: disable=C0413
 
 # BIBLIOTECAS: NATIVAS
-import asyncio
-import sys
-import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import asyncio, sys, os, logging, random, string, json
 from datetime import datetime
-import logging
-import random
-import string
 
 # BIBLIOTECAS: NECESSÁRIO INSTALAR → pip install brotli mitmproxy
-from flask import Flask, request, jsonify
 from g4f.client import Client
 
 # FUNÇÕES DE ARQUIVOS
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "resources")))
 from chatNew import chatNew
 from chatAddMessage import chatAddMessage
 from historyChat import historyChat
 from historyMessages import historyMessages
 from historyDelete import historyDelete
 
-
-# OCULTAR CONSOLE DE ERROS G4F
-if sys.platform.startswith("win"):
+try:
+    # OCULTAR CONSOLE DE ERROS G4F
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-
-# OCULTAR CONSOLE DO SERVIDOR
-logging.getLogger("werkzeug").disabled = True
-
-app = Flask(__name__)
-client = Client()
+    # INICIAR O SERVIDOR G4F
+    client = Client()
 
 
-@app.route("/chat", methods=["POST"])
-def chat():
-    """IGNORE"""
-    try:
-        data = request.json
-    except Exception:
-        infBody = {"ret": False, "msg": "CHAT: ERRO | JSON INVALIDO"}
-        return jsonify(infBody), 200
-
-    # DEFINIR 'action'
-    action = data.get("action")
-    if action is None or action == "":
-        infBody = {"ret": False, "msg": "CHAT: ERRO | INFORMAR O 'action'"}
-        return jsonify(infBody), 200
-
-    # DEFINIR 'message'
-    message = data.get("message")
-    if message is None or message == "":
-        message = None
-
-    # DEFINIR 'model'
-    model = data.get("model")
-    if model is None or model == "":
-        model = "gpt-3.5-turbo"
-
-    # DEFINIR 'chatId'
-    chatId = data.get("chatId")
-    if chatId is None or chatId == "":
-        chatId = None
-
-    # DEFINIR 'includesInMessages'
-    includesInMessages = data.get("includesInMessages")
-    if includesInMessages is None or includesInMessages == "":
-        includesInMessages = []
-
-    # DEFINIR 'qtdDeleteMessages'
-    qtdDeleteMessages = data.get("qtdDeleteMessages")
-    if qtdDeleteMessages is None or qtdDeleteMessages == "":
-        qtdDeleteMessages = 0
-
-    # ACTION: ENVIAR MENSAGEM
-    if action == "messageSend":
-        message = data.get("message")
-        if message is None:
-            infBody = {
-                "ret": False,
-                "msg": "CHAT [MESSAGESEND]: ERRO | INFORMAR O 'message'",
-            }
-            return jsonify(infBody), 200
-
-        isNewChat = False
-        messagesToAssistant = []
-        if chatId is None:
-            # GERAR 'chatId' → ANO_MES_DIA-HOR.MIN.SEC.MIL-ABC
-            letras = "".join(random.choices(string.ascii_uppercase, k=3))
-            chatId = f"{datetime.now().strftime("%Y_%m_%d-%H.%M.%S.%f")[:-3]}-{letras}"
-            isNewChat = True
-        else:
-            # ID DO CHAT NÃO EXISTE
-            infHistoryChat = {
-                "chatId": chatId,
-                "includesInMessages": ["role", "content"],
-            }
-            messagesToAssistant = historyMessages(infHistoryChat)
-            if not messagesToAssistant["ret"]:
-                return jsonify(messagesToAssistant), 200
-            messagesToAssistant = messagesToAssistant["res"][0]["messages"]
-
-        # BOT: ENVIAR MENSAGEM
-        messagesToAssistant.append({"role": "user", "content": message})
-        timestampUser = int(datetime.now().timestamp())
+    # REQUISIÇÕES 
+    def request(data):
+        """IGNORE"""
         try:
-            response = client.chat.completions.create(
-                model=model, messages=messagesToAssistant
-            )
-        except Exception as e:
-            print(str(e))
-            infBody = {
-                "ret": False,
-                "msg": "CHAT [MESSAGESEND]: ERRO | INFORMAR O 'message'",
-            }
-            return jsonify(infBody), 200
-        # BOT: RESPOSTA RECEBIDA
-        if response:
-            resposta = response.choices[0].message.content
-            timestampAssistant = int(datetime.now().timestamp())
-            # CHAT: CRIAR NOVO
-            infChat = {
-                "chatId": chatId,
-                "model": model,
-                "timestampUser": timestampUser,
-                "timestampAssistant": timestampAssistant,
-                "message": message,
-                "response": resposta,
-            }
-            if isNewChat:
-                chatNew(infChat)
+            data = json.loads(data)
+        except Exception:
+            infBody = {"ret": False, "msg": "CHAT: ERRO | JSON INVALIDO"}
+            return json.dumps(infBody), 200
+
+        # DEFINIR 'action'
+        action = data.get("action")
+        if action is None or action == "":
+            infBody = {"ret": False, "msg": "CHAT: ERRO | INFORMAR O 'action'"}
+            return json.dumps(infBody), 200
+
+        # DEFINIR 'message'
+        message = data.get("message")
+        if message is None or message == "":
+            message = None
+
+        # DEFINIR 'model'
+        model = data.get("model")
+        if model is None or model == "":
+            model = "gpt-3.5-turbo"
+
+        # DEFINIR 'chatId'
+        chatId = data.get("chatId")
+        if chatId is None or chatId == "":
+            chatId = None
+
+        # DEFINIR 'includesInMessages'
+        includesInMessages = data.get("includesInMessages")
+        if includesInMessages is None or includesInMessages == "":
+            includesInMessages = []
+
+        # DEFINIR 'qtdDeleteMessages'
+        qtdDeleteMessages = data.get("qtdDeleteMessages")
+        if qtdDeleteMessages is None or qtdDeleteMessages == "":
+            qtdDeleteMessages = 0
+
+        # ACTION: ENVIAR MENSAGEM
+        if action == "messageSend":
+            message = data.get("message")
+            if message is None:
+                infBody = {
+                    "ret": False,
+                    "msg": "CHAT [MESSAGESEND]: ERRO | INFORMAR O 'message'",
+                }
+                return json.dumps(infBody), 200
+
+            isNewChat = False
+            messagesToAssistant = []
+            if chatId is None:
+                # GERAR 'chatId' → ANO_MES_DIA-HOR.MIN.SEC.MIL-ABC
+                letras = "".join(random.choices(string.ascii_uppercase, k=3))
+                chatId = f"{datetime.now().strftime("%Y_%m_%d-%H.%M.%S.%f")[:-3]}-{letras}"
+                isNewChat = True
             else:
-                # CHAT: ADICIONAR AO EXISTENTE
-                chatAddMessage(infChat)
-            infBody = {
-                "ret": True,
-                "msg": "CHAT [MESSAGESEND]: OK",
-                "res": {
-                    "chatNew": isNewChat,
+                # ID DO CHAT NÃO EXISTE
+                infHistoryChat = {
+                    "chatId": chatId,
+                    "includesInMessages": ["role", "content"],
+                }
+                messagesToAssistant = historyMessages(infHistoryChat)
+                if not messagesToAssistant["ret"]:
+                    return json.dumps(messagesToAssistant), 200
+                messagesToAssistant = messagesToAssistant["res"][0]["messages"]
+
+            # BOT: ENVIAR MENSAGEM
+            messagesToAssistant.append({"role": "user", "content": message})
+            timestampUser = int(datetime.now().timestamp())
+            try:
+                response = client.chat.completions.create(
+                    model=model, messages=messagesToAssistant
+                )
+            except Exception as e:
+                print(str(e))
+                infBody = {
+                    "ret": False,
+                    "msg": "CHAT [MESSAGESEND]: ERRO | INFORMAR O 'message'",
+                }
+                return json.dumps(infBody), 200
+            # BOT: RESPOSTA RECEBIDA
+            if response:
+                resposta = response.choices[0].message.content
+                timestampAssistant = int(datetime.now().timestamp())
+                # CHAT: CRIAR NOVO
+                infChat = {
                     "chatId": chatId,
                     "model": model,
+                    "timestampUser": timestampUser,
+                    "timestampAssistant": timestampAssistant,
+                    "message": message,
                     "response": resposta,
-                },
-            }
-            return jsonify(infBody), 200
+                }
+                if isNewChat:
+                    chatNew(infChat)
+                else:
+                    # CHAT: ADICIONAR AO EXISTENTE
+                    chatAddMessage(infChat)
+                infBody = {
+                    "ret": True,
+                    "msg": "CHAT [MESSAGESEND]: OK",
+                    "res": {
+                        "chatNew": isNewChat,
+                        "chatId": chatId,
+                        "model": model,
+                        "response": resposta,
+                    },
+                }
+                return json.dumps(infBody), 200
+            else:
+                # BOT: RESPOSTA NÃO RECEBIDA
+                infBody = {
+                    "ret": False,
+                    "msg": "CHAT [MESSAGESEND]: ERRO | BOT NÃO RESPONDEU",
+                }
+                return json.dumps(infBody), 200
+
+        # ACTION: HISTÓRICO DE CHATS
+        elif action == "historyChat":
+            infHistoryChat = {"model": model, "includesInMessages": includesInMessages}
+            retHistoryChat = historyChat(infHistoryChat)
+            return json.dumps(retHistoryChat), 200
+
+        # ACTION: HISTÓRICO DE MENSAGENS
+        elif action == "historyMessages":
+            infHistoryChat = {"chatId": chatId, "includesInMessages": includesInMessages}
+            retHistoryMessages = historyMessages(infHistoryChat)
+            return json.dumps(retHistoryMessages), 200
+
+        # ACTION: DELETAR MENSAGENS DO CHAT
+        elif action == "historyDelete":
+            infHistoryDelete = {"chatId": chatId, "qtdDeleteMessages": qtdDeleteMessages}
+            retHistoryDelete = historyDelete(infHistoryDelete)
+            return json.dumps(retHistoryDelete), 200
+
+        # ACTION: NÃO IDENTIFICADA
         else:
-            # BOT: RESPOSTA NÃO RECEBIDA
-            infBody = {
-                "ret": False,
-                "msg": "CHAT [MESSAGESEND]: ERRO | BOT NÃO RESPONDEU",
-            }
-            return jsonify(infBody), 200
-
-    # ACTION: HISTÓRICO DE CHATS
-    elif action == "historyChat":
-        infHistoryChat = {"model": model, "includesInMessages": includesInMessages}
-        retHistoryChat = historyChat(infHistoryChat)
-        return jsonify(retHistoryChat), 200
-
-    # ACTION: HISTÓRICO DE MENSAGENS
-    elif action == "historyMessages":
-        infHistoryChat = {"chatId": chatId, "includesInMessages": includesInMessages}
-        retHistoryMessages = historyMessages(infHistoryChat)
-        return jsonify(retHistoryMessages), 200
-
-    # ACTION: DELETAR MENSAGENS DO CHAT
-    elif action == "historyDelete":
-        infHistoryDelete = {"chatId": chatId, "qtdDeleteMessages": qtdDeleteMessages}
-        retHistoryDelete = historyDelete(infHistoryDelete)
-        return jsonify(retHistoryDelete), 200
-
-    # ACTION: NÃO IDENTIFICADA
-    else:
-        infBody = {"ret": False, "msg": "CHAT: ERRO | NÃO IDENTIFICADO 'action'"}
-        return jsonify(infBody), 200
+            infBody = {"ret": False, "msg": "CHAT: ERRO | NÃO IDENTIFICADO 'action'"}
+            return json.dumps(infBody), 200
 
 
-##### INICIAR SERVIDOR
-if __name__ == "__main__":
-    # LIMPAR CONSOLE
-    os.system("cls")
-    print("RODANDO NA PORTA")
-    app.run(port=8890, debug=True)
+
+# ------------------------------------------ SERVIDOR HTTP ------------------------------------------
+    class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+        def _set_headers(self, status_code=200):
+            self.send_response(status_code)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+        def do_POST(self):
+            if self.path == "/chat":
+                response_body, status_code = request(self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8'))
+                self._set_headers(status_code)
+                self.wfile.write(response_body.encode('utf-8'))
+        def log_message(self, format, *args):
+            pass
+    def serverRun():
+        logging.getLogger('http.server').setLevel(logging.CRITICAL)
+        os.system("cls")
+        port=8890
+        print(f'RODANDO NA PORTA: {port}')
+        HTTPServer(('', port), SimpleHTTPRequestHandler).serve_forever()
+
+    # #### INICIAR SERVIDOR
+    if __name__ == "__main__":
+        serverRun()
+
+
+
+except Exception:
+    print('ERRO GERAL PYTHON')
+    os._exit(1)
---
+++ src/z_Outros_server/2_SCRIPT.bat
@@ -0,0 +1,63 @@
+@chcp 65001 & @echo off & setlocal enabledelayedexpansion
+set "letra=%~d0" & set "local=%~dp0"
+set "letra=%letra:~0,1%" & set "local=%local:~0,-1%" & set "arquivo=%~nx0" & set "argString=%*"
+set "usuario=%USERNAME%" & set "argTUDO=%~1 %~2 %~3 %~4 %~5" & set "arg1=%~1" & set "arg2=%~2" & set "arg3=%~3" & set "arg4=%~4" & set "arg5=%~5"
+
+rem AVISO PARA USAR O ATALHO COM PARAMENTROS
+if "!arg1!" equ "" !fileMsg! "[!local!\!arquivo!]\n\nNao usar o BAT/BACKGROUND" & exit
+
+rem set "start=ERRO" & set "adm=ERRO" & NET SESSION >nul 2>&1 & if !errorlevel! neq 0 ( set "adm=NAO" ) else ( set "adm=SIM" )
+
+rem echo WScript.Echo(new Date().getTime()); > !temp!\time.js & for /f "delims=" %%a in ('cscript //nologo !temp!\time.js') do set "timeNow=%%a"
+rem set "timeNow=!timeNow:~0,-3!" & set "dia=!DATE:~0,2!" & set "mes=!DATE:~3,2!"
+
+rem →→→ COMO USAR: Definir o 'mode'
+rem Exemplo 1: # WebScraper # criar a pasta: '.\src\z_Outros_serverC6' → o arquivo a ser executado sera o '.\src\serverC6.js'
+rem Exemplo 1: # WebScraper # criar a copia do programExe: 'nodeWebScraper_serverC6.exe'
+rem Exemplo 2: # WebScraper # criar a pasta: '.\src\z_Outros_serverJucesp' → o arquivo a ser executado sera o '.\src\serverJucesp.js'
+rem Exemplo 2: # WebScraper # criar a copia do programExe: 'nodeWebScraper_serverJucesp.exe'
+
+rem MODE →→→ 'CMD' (RESTART [SIM]) / 'LEGACY' (RESTART [NAO]) # PROJECT | OUTROSADD | ARQUIVO SCRIPT
+for /f "tokens=1,2,3,4,5,6 delims=\" %%a in ("!local!") do ( set "project=%%d" & set "outrosAdd=%%f" ) & set "replace="
+set "outrosAdd=!outrosAdd:z_Outros_=%replace%!" & set "scriptType=ERRO" & set "ret=ERRO"
+set "mode=CMD" & set "programExe=python" & set "root=!letra!:\ARQUIVOS\PROJETOS" & set "fileScript=!root!\!project!\src\!outrosAdd!" & cd\ & !letra!: & cd !root!\!project!
+rem #### ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ ##########################################################
+
+rem CHECAR SE ESTA RODANDO
+tasklist /fi "ImageName eq !programExe!!project!_!outrosAdd!.exe" /fo csv 2>NUL | find /I "!programExe!!project!_!outrosAdd!.exe">NUL
+if "%ERRORLEVEL%"=="0"  ( set "ret=TRUE" ) else ( set "ret=FALSE" )
+
+rem ESTA RODANDO [NAO]
+rem if "!ret!"=="FALSE" ( if "!arg1!"=="!arg1:OFF=!" ( ) )
+
+rem ESTA RODANDO [SIM]
+rem if "!ret!"=="TRUE" ( )
+
+rem  (NAO SUBIR OS 'if'!!!)
+if "!mode!"=="CMD" ( set "scriptType=processCmdKeep" ) else ( if "!mode!"=="LEGACY" ( set "scriptType=processCmdKeep" ) )
+if "!scriptType!" equ "ERRO" !fileMsg! "[!local!\!arquivo!]\n\n'mode' deve ser\n'CMD', 'LEGACY'" & exit
+endlocal & call "%fileChrome_Extension%\src\scripts\BAT\%scriptType%.bat" "%arg1%_WINTP2" "%project%@%outrosAdd%" "%fileScript%" "%mode%" "%programExe%" "%ret%" & setlocal enabledelayedexpansion
+set "ret=%ret2%"
+rem #####################################################################
+
+rem APENAS ENCERRAR E NAO CONTINUAR O BAT
+if "%~2"=="FORCE_STOP" ( exit )
+
+exit
+exit
+
+rem TIMESTAMP ATUAL (OBRIGATORIO FICAR APOS O CALL!!!)
+rem echo WScript.Echo(new Date().getTime()); > !temp!\time.js & for /f "delims=" %%a in ('cscript //nologo !temp!\time.js') do set "timeNow=%%a"
+rem set "timeNow=!timeNow:~0,-3!" & set "dia=!DATE:~0,2!" & set "mes=!DATE:~3,2!"
+
+rem ESTAVA RODANDO [SIM]
+rem if "!ret!"=="TRUE" ( )
+
+rem ESTAVA RODANDO [NAO]
+rem if "!ret!"=="FALSE" ( )
+
+exit
+exit
+
+
+
